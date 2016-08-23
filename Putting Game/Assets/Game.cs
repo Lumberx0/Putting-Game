e34861ffaba8e06cefd84d08ef6e644e956d4da7
Lumberx0 <deleeuw.alexander@icloud.com>
2016-08-24 01:06:43 +0200

    Commit1
    MainMenu

---
+++ Putting Game/Assets/Game.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Game : EventTrigger, IDragHandler {
+
+
+	void Update() {
+	}
+}
---
+++ Putting Game/Assets/LevelController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour {
+
+	public void levelSwitch (string levelName) {
+
+	SceneManager.LoadScene(levelName);
+
+	}
+
+	public void quitGame () {
+		Application.Quit();
+	}
+}
