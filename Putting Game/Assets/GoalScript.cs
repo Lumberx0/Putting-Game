dc0c00a01ee6c41f09b66ee5b9001cc9b33f46a7
Lumberx0 <deleeuw.alexander@icloud.com>
2016-08-28 00:56:18 +0200

    Commit5

---
+++ Putting Game/Assets/GoalScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GoalScript : MonoBehaviour {
+
+public Text text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Goal") {
+			text.text = "Level Completed!";
+			Die();
+		}
+
+	}
+	void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Putting Game/Assets/Scripts/Game.cs
+++ Putting Game/Assets/Scripts/Game.cs
@@ -1,10 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Game : EventTrigger, IDragHandler {
+public class Game : EventTrigger {
 
+Vector3 mMouseDownPos;
+Vector3 mMouseUpPos;
+public float speed = 1f;
+public Rigidbody2D rb;
+public Text text;
 
-	void Update() {
+	void OnMouseDown() {
+
+		mMouseDownPos = Input.mousePosition;
+     	Debug.Log( "the mouse down pos is " + mMouseDownPos.z.ToString() );
+     	mMouseDownPos.z = 0;
+
 	}
+	void OnMouseUp ()
+	{
+		mMouseUpPos = Input.mousePosition;
+     	mMouseUpPos.z = 0;
+     	var direction = mMouseDownPos - mMouseUpPos;
+     	direction.Normalize();
+     	rb.AddForce (direction * speed);
+     	Debug.Log( "the mouse up pos is " + mMouseUpPos.ToString() );
+	}
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+
+
 }
